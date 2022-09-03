c5e22b6448d3799573384fb96c2a724bfc15b25b
taynarasoria <taynarasoria@alu.ufc.br>
2022-09-03 16:35:13 -0300

    finalizando

--- main.js
+++ main.js
@@ -14,62 +14,70 @@ request.onreadystatechange = function(){
 
 
 //VARIAVEIS
-var pontos = 0;
+var escores = 0;
 var aux = -1;
-var inp = document.getElementsByTagName("input");
+var resp = document.getElementsByTagName("input");
+let btn = document.getElementByTagName("confirmar");
 
-//QUESTOES
-function mostrarQuestao() {
-
-    document.getElementById("resultado").innerHTML = "";
-    document.getElementById("listaRespostas").style.display = "block";        
-
-    if(aux === -1 || inp[0].checked != false || inp[1].checked != false || inp[2].checked != false || inp[3].checked != false){
-        if(aux != -1){
-            for(var i=0 ; i<form[aux].options.length ; i++){
-                pontos = pontos + inp[i].checked*form[aux]['options'][i]['value']
-            }
-        }
-        document.getElementById("confirmar").innerHTML = "Próximo";
-        aux++;
-    }
-
-    if(aux == -1){
-        document.getElementById("titulo").classList.remove('hide');
-        document.getElementById("resultado").remove('hide');
-    }
-
-    if(aux < form.length){
-        document.getElementById("titulo").innerHTML=form[aux].title;
-        
-        for(var i=0 ; i<form[aux].options.length ; i++){
-            if(inp[i].checked === true){
-                inp[i].checked = false;
-            }
-            document.getElementsByTagName("span")[i].innerHTML=form[aux].options[i].answer
-            inp[i].value=form[aux].options[i].value
-        }
-        
-    }
+//MOSTRANDO A QUESTAO
+function mostrarQuestao() { 
+    document.getElementById("resultado").innerHTML = ""; 
+    document.getElementById("listaRespostas").style.display = "block"; 
     
-    else{
-        console.log(form[aux-1].options.length)
-        finalizarQuiz();
+    if(aux === -1 || resp[0].checked != false || resp[1].checked != false || resp[2].checked != false || resp[3].checked != false){
+      if(aux != -1){
+        for(var i =0; i < form[aux].options.length; i++){
+           escores = escores + resp[i].checked*form[aux]['options'][i]['value']
+        }
     }
-}
-
-//FINALIZANDO
-function finalizarQuiz() {
-  var pont = 3*(form[aux-1].options.length+1);
-
-  document.getElementById("listaRespostas").style.display = "none";
+      document.getElementById("confirmar").textContent = "Próxima pergunta"; 
+      aux++;
+      proxQuestao(); 
+    };
+      };
   
-  document.getElementById("confirmar").innerHTML = "Refazer quiz";
-
-  document.getElementById("titulo").innerHTML = "QUIZ DOS VALORES DA GTI";
-
-  document.getElementById("resultado").innerHTML = "Sua pontuação: " + (pontos*100/pont) + "%";
-
-}
-
-
+  btn.addEventListener('click', mostrarQuestao, false);
+  
+    function reiniciar(){
+        aux = -1;
+       escores = 0;
+        for(let i = 0; i<form[aux].options[i].value;i++){
+          document.location.reload(true);
+        }
+    };
+  
+          
+      function proxQuestao(){
+        if(aux == -1){  
+          document.getElementById("titulo").classList.remove('hide');
+          document.getElementById("resultado").remove('hide');  
+        }; 
+    
+      if(aux < form.length){  
+          document.getElementById("titulo").innerHTML=form[aux].title;  
+            
+          for(var i=0 ; i < form[aux].options.length ; i++){  
+              if(resp[i].checked === true){  
+                  resp[i].checked = false;  
+                }  
+              document.getElementsByTagName("span")[i].innerHTML=form[aux].options[i].answer  
+              resp.value=form[aux].options[i].value  
+            };  
+            
+        } 
+  
+        
+      else{  
+            console.log(form[aux-1].options.length)
+            finalizarQuiz();  
+        }  
+      }; 
+   
+       //FINALIZANDO
+      function finalizarQuiz() {
+        var pont = 3*(form[aux-1].options.length+1);
+          document.getElementById("confirmar").innerHTML = "Refazer quiz";
+          document.getElementById("listaRespostas").style.display = "none"; 
+          document.getElementById("resultado").innerHTML = "Sua pontuação: " + (pontos*100/pont) + "%";   
+          btn.addEventListener('click', reiniciar(), false); 
+      }
